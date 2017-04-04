ac15e8d1adb4ab4a79b4c2b16951dd7f1e127869
chakalakka <a.s.taudt@umcg.nl>
2017-04-04 17:41:59 +0200

    added export function

---
+++ R/export.R
@@ -0,0 +1,26 @@
+#' Export a methylome
+#' 
+#' Export a methylome as a TSV file.
+#' 
+#' @param model A \code{\link{methimputeBinomialHMM}} object.
+#' @param filename The name of the file to be exported.
+#' @return \code{NULL}
+#' @export
+#' @examples
+#'## Get some toy data
+#'file <- system.file("data","arabidopsis_toydata.RData", package="methimpute")
+#'data <- get(load(file))
+#'print(data)
+#'model <- callMethylation(data, max.iter=10)
+#'exportMethylome(model, filename = tempfile())
+#'
+exportMethylome <- function(model, filename) {
+  
+    ptm <- startTimedMessage("Writing to file ", filename, " ...")
+    data <- model$data
+    df <- methods::as(data, 'data.frame')
+    df <- df[,c('seqnames', 'start', 'strand', 'context', 'counts.methylated', 'counts.total', 'posteriorMax', 'posteriorMeth', 'posteriorUnmeth', 'status')]
+    write.table(df, file = filename, quote = FALSE, sep = '\t', row.names = FALSE, col.names = TRUE)
+    stopTimedMessage(ptm)
+    
+}
